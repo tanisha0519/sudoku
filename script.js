73cf37b26ad0fb34ecdb5d813ea3f21a70d68e46
tanisha0519 <tanishas1910@gmail.com>
2026-06-28 15:14:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const puzzle = [
+    {
 [8,0,0,2,0,0,4,0,0],
 [1,0,4,3,0,0,0,0,5],
 [0,0,0,0,0,5,0,2,0],
@@ -8,8 +9,8 @@ const puzzle = [
 [0,5,0,4,0,0,0,0,0],
 [3,0,0,0,0,2,9,0,7],
 [0,0,6,0,0,8,0,0,3]
-];
-const solution = [
+],
+solution = [
 [8,3,5,2,7,1,4,9,6],
 [1,2,4,3,6,9,8,7,5],
 [7,6,9,8,4,5,3,2,1],
@@ -19,7 +20,11 @@ const solution = [
 [9,5,2,4,3,7,1,6,8],
 [3,1,8,6,5,2,9,4,7],
 [4,7,6,9,1,8,2,5,3]
+]
+}
 ];
+let currentPuzzle;
+let currentSolution;
 function createBoard(){
     const board = document.getElementById("board");
     board.innerHTML = "";
@@ -31,8 +36,8 @@ function createBoard(){
             cell.addEventListener("input", function () {
                 this.value = this.value.replace(/[^1-9]/g, "");
             });
-            if(puzzle[r][c] != 0){
-                cell.value = puzzle[r][c];
+            if(currentPuzzle[r][c] != 0){
+                cell.value = currentPuzzle[r][c];
                 cell.disabled = true;
                 cell.classList.add("fixed");
             }
@@ -40,33 +45,33 @@ function createBoard(){
         }
     }
 }
+function newGame(){
+    let randomIndex = Math.floor(Math.random() * games.length);
+    currentPuzzle = games[randomIndex].puzzle;
+    currentSolution = games[randomIndex].solution;
+    createBoard();
+    document.getElementById("message").innerHTML = "";
+}
 function checkSolution(){
-    let correct=true;
-    for(let r=0;r<9;r++){
-        for(let c=0;c<9;c++){
-            let cell=document.getElementById(r+"-"+c);
-            if(Number(cell.value)!=solution[r][c]){
-                correct=false;
+    let correct = true;
+    for(let r = 0; r < 9; r++){
+        for(let c = 0; c < 9; c++){
+            let cell = document.getElementById(r + "-" + c);
+            if(Number(cell.value) !== currentSolution[r][c]){
+                correct = false;
             }
         }
     }
-    if(correct){
-        document.getElementById("message").innerHTML=" 😍 You solved it !!!!......";
-    }else{
-        document.getElementById("message").innerHTML=" Oopss...you are a bit incorrect!!... 😶‍🌫️ ";
-    }
-
+    document.getElementById("message").innerHTML =
+        correct
+        ? "😍 You solved it !!!!......"
+        : "Oopss...you are a bit incorrect!!... 😶‍🌫️";
 }
 function showSolution(){
-    for(let r=0;r<9;r++){
-        for(let c=0;c<9;c++){
-            let cell=document.getElementById(r+"-"+c);
-            cell.value=solution[r][c];
+    for(let r = 0; r < 9; r++){
+        for(let c = 0; c < 9; c++){
+            document.getElementById(r + "-" + c).value = currentSolution[r][c];
         }
     }
 }
-function newGame(){
-    createBoard();
-    document.getElementById("message").innerHTML="";
-}
-createBoard();
+newGame();
